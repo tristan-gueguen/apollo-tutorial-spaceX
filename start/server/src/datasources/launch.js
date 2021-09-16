00eb063d291d1ba540b6d7c5ac09d26c7d57c5a3
tristan-gueguen <tristan.gueguen@gmail.com>
2021-09-16 13:22:21 +0200

    cursor is now launch id or launch date

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -27,7 +27,7 @@ class LaunchAPI extends RESTDataSource {
   launchReducer(launch) {
     return {
       id: launch.flight_number || 0,
-      cursor: `${launch.launch_date_unix}`,
+      cursor: `${launch.flight_number || launch.launch_date_unix}`,
       site: launch.launch_site && launch.launch_site.site_name,
       mission: {
         name: launch.mission_name,
